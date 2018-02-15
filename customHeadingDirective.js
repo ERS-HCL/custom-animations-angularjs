77e79a9dba0d52c1bfba3c96b4e55a3ca086f8f9
harika12345 <36506029+harika12345@users.noreply.github.com>
2018-02-15 18:56:20 +0530

    Add files via upload

---
+++ customHeadingDirective.js
@@ -0,0 +1,130 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+app.directive('customHeading',function(customBackground,$timeout,reusableComponent,slideAnimation){
+   return{
+        restrict:'E',
+        templateUrl:function(ele,attr){
+            console.log('setting Template');
+            var tempUrl = 'HeadingTemplate.html';
+            return tempUrl;
+        },
+        
+        transclude:true,
+        link:function($scope,ele,attr){
+            console.log('custom directive');
+            var temp = angular.element(ele).children();
+            var targetDiv = angular.element(ele).children().children();
+           
+
+            targetDiv.css($scope.stylesForHeading);
+            targetDiv.css({
+                'position':'relative'                
+            });           
+
+            var dataContainer = angular.element($('.dataContainer'));
+            dataContainer.css({
+                'position':'absolute',                
+                'z-index':'5',
+                'height':'0'
+            });            
+            
+            
+            var btn = angular.element($('<input type="button"/>'));
+            temp.append(btn);
+            btn.attr('value',"Animate");
+            console.log(attr.params);
+            console.log(attr.animationeffect);   
+            
+            var dimensionsOftargetDiv = reusableComponent.dimensionsWithContent(targetDiv);
+            var outerDimensionsOftargetDiv = reusableComponent.dimensionsWithPaddingAndBorder(targetDiv);
+            temp.css({
+                'height':outerDimensionsOftargetDiv.height+'px',
+                'position':'relative'
+            });
+            
+            
+            var params = JSON.parse(attr.params);
+            angular.element(btn).on('click',function(){
+               // alert('gf');
+                if(attr.animationeffect == 'backgroundTransition'){
+                var backgroundDiv = angular.element('<div></div>');
+                backgroundDiv.css({
+                    'position':'absolute',
+                    'z-index':'3'                
+                });
+                
+                targetDiv.append(backgroundDiv);
+                var desiredBackgroundHeight = dimensionsOftargetDiv.height;
+                var desiredBackgroundWidth = dimensionsOftargetDiv.width;
+                console.log(desiredBackgroundHeight + ' '+ desiredBackgroundWidth); 
+                var backgroundPadding =  reusableComponent.extractPaddingPropertyFromElement(targetDiv);
+                var backgroundProperties = {};
+               
+               var paddingProperties = backgroundPadding;
+                
+                $timeout(function(){
+                    customBackground.setBackground(backgroundDiv,params,desiredBackgroundHeight,desiredBackgroundWidth,paddingProperties);
+                   
+                },1000);                
+            }         
+            else if(attr.animationeffect == 'SlideDown'){
+                var paddingProperties = reusableComponent.extractPaddingPropertyFromElement(targetDiv);
+                //reusableFunctions.setOverflowProperty(targetDiv,'hidden');
+                targetDiv.css({
+                    'display':'none'                    
+                });
+              /*  $timeout(function(){
+                    targetDiv.css({                        
+                        'visibility':'visible'
+                    });                   
+                    customizedBackgroundEffect.setBackground(targetDiv,params,targetDivHeight,targetDivWidth);
+                    $timeout(function(){
+                        reusableFunctions.setOverflowProperty(targetDiv,'visible');
+                    },params.speed);
+                },3000);*/
+                
+                    targetDiv.slideDown(params.speed);
+               
+            }
+            else if(attr.animationeffect == 'SlideUp'){              
+                targetDiv.slideUp(params.speed);             
+            }
+            else if(attr.animationeffect == "SlideLeftDown"){              
+                slideAnimation.setLeftSlideDown(targetDiv,params);
+               
+            }
+            else if(attr.animationeffect == 'SlideLeftUp'){              
+                slideAnimation.setLeftSlideUp(targetDiv,params);                           
+            }
+            
+            else if(attr.animationeffect == 'SlideRightDown'){               
+                slideAnimation.setRightSlideDown(targetDiv,params);                             
+            }
+            
+            else if(attr.animationeffect == 'SlideRightUp'){               
+                slideAnimation.setRightSlideUp(targetDiv,params);                
+            }
+            
+            else if(attr.animationeffect == 'SlideBottomDown'){               
+                slideAnimation.setBottomSlideDown(targetDiv,params);               
+            }
+            
+            else if(attr.animationeffect == 'SlideBottomUp'){              
+                slideAnimation.setBottomSlideUp(targetDiv,params);               
+            }            
+            
+           });
+            
+        }  
+        
+   }    
+});
+
+
+
+
